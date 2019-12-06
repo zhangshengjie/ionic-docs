3141477eba4426a6aa7ad94efdccd91704a871c7
榊原昌彦 <sakakibara@rdlabo.jp>
2019-12-06 18:52:05 +0900

    chore: 変更箇所の絞り込み

--- src/components/page-footer/page-footer.tsx
+++ src/components/page-footer/page-footer.tsx
@@ -22,20 +22,20 @@ export class DocsPageFooter {
       return null;
     }
 
-    // const {
-    //   path,
-    //   lastUpdated
-    // } = page.github;
+    const {
+      path,
+      lastUpdated
+    } = page.github;
 
     // merge and dedupe contributor data
-    // const contributors = Array.from(
-    //   new Set([...page.github.contributors || [], ...page.contributors || []])
-    // );
+    const contributors = Array.from(
+      new Set([...page.github.contributors || [], ...page.contributors || []])
+    );
 
     // const editHref = `https://github.com/ionic-jp/ionic-docs/edit/master/${path}`;
-    // const updatedHref = `https://github.com/ionic-jp/ionic-docs/commits/master/${path}`;
-    // const updatedText = lastUpdated ? new Date(lastUpdated).toISOString().slice(0, 10) : null;
-    // const contributorHref = (contributor) => `${updatedHref}?author=${contributor}`;
+    const updatedHref = `https://github.com/ionic-jp/ionic-docs/commits/master/${path}`;
+    const updatedText = lastUpdated ? new Date(lastUpdated).toISOString().slice(0, 10) : null;
+    const contributorHref = (contributor) => `${updatedHref}?author=${contributor}`;
 
     const paggination = (
       page.previousText && page.previousUrl || page.nextText && page.nextUrl
@@ -45,11 +45,11 @@ export class DocsPageFooter {
 
     return [
       paggination,
-      // <div class="page-footer__row">
-      //  {/*{contributors.length ? <contributor-list contributors={contributors} link={contributorHref}/> : null}*/}
-      //  <docs-button round href={editHref}>翻訳する <ForwardArrow/></docs-button>
-      //  {updatedText ? <a class="last-updated" href={updatedHref}>Updated {updatedText}</a> : ''}
-      // </div>
+      <div class="page-footer__row">
+       {contributors.length ? <contributor-list contributors={contributors} link={contributorHref}/> : null}
+       {/*<docs-button round href={editHref}>翻訳する <ForwardArrow/></docs-button>*/}
+       {updatedText ? <a class="last-updated" href={updatedHref}>Updated {updatedText}</a> : ''}
+      </div>
     ];
   }
 }
