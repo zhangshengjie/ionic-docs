26e156a96b141c88da22bf037fb16575efe02685
榊原昌彦 <sakakibara@rdlabo.jp>
2019-12-08 10:55:20 +0900

    feat: scriptsの整理

--- src/components/footer-announcement/footer-announcement.tsx
+++ src/components/footer-announcement/footer-announcement.tsx
@@ -29,10 +29,7 @@ export class DocsFooterAnnouncement {
     return (
       <footer class="footer-announcement">
         <p>
-          <strong>Ionic Meetup</strong>Ionicについての知識を共有するためのイベント。初参加・初登壇歓迎。
-          <a href="https://ionic-jp.connpass.com/event/155092/" target="_blank">12/7 兵庫
-            <svg viewBox="0 0 512 512"><path d="M294.1 256L167 129c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.3 34 0L345 239c9.1 9.1 9.3 23.7.7 33.1L201.1 417c-4.7 4.7-10.9 7-17 7s-12.3-2.3-17-7c-9.4-9.4-9.4-24.6 0-33.9l127-127.1z"></path></svg>
-          </a>
+          <strong>Ionic Meetup</strong>Ionicについての知識を共有するためのイベント。初参加・初登壇大歓迎。
           <a href="https://ionic-jp.connpass.com/event/155536/" target="_blank">
             12/14 東京
             <svg viewBox="0 0 512 512"><path d="M294.1 256L167 129c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.3 34 0L345 239c9.1 9.1 9.3 23.7.7 33.1L201.1 417c-4.7 4.7-10.9 7-17 7s-12.3-2.3-17-7c-9.4-9.4-9.4-24.6 0-33.9l127-127.1z"></path></svg>
